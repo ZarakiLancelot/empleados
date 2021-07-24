2870bcea3460b4548311a6946882d70c4e197e75
Edwin Vasquez <edwin.vasquez@grupopdc.com>
2021-07-23 19:02:33 -0600

    configuración de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDmpaEXycmuuwoY_bKSQ4ha0YHxYx_ZDPw",
+    authDomain: "empleados-c8613.firebaseapp.com",
+    projectId: "empleados-c8613",
+    storageBucket: "empleados-c8613.appspot.com",
+    messagingSenderId: "78353861361",
+    appId: "1:78353861361:web:3b007ad80d2ba9e53eb750"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDmpaEXycmuuwoY_bKSQ4ha0YHxYx_ZDPw",
+    authDomain: "empleados-c8613.firebaseapp.com",
+    projectId: "empleados-c8613",
+    storageBucket: "empleados-c8613.appspot.com",
+    messagingSenderId: "78353861361",
+    appId: "1:78353861361:web:3b007ad80d2ba9e53eb750"
+  }
 };
 
 /*
